910e9a8619badd710bc3195e284cf5dd1114972d
Andrius <andriusjavait@gmail.com>
2017-02-01 21:05:07 +0000

    update MyController code

--- Chapter8_1/src/com/example/servlet/MyController.java
+++ Chapter8_1/src/com/example/servlet/MyController.java
@@ -1,11 +1,24 @@
 package com.example.servlet;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import foo.other.*;
 
 public class MyController extends HttpServlet {
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response)throws ServletException,IOException{
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// get parameter from form:
+		String p_name = request.getParameter("person_name");
+		// create new Person object with this form parameter:
+		Person person = new Person(p_name);
+		// save this person object to request scope attribute:
+		request.setAttribute("person_object", person);
+		// using request create RequestDispatcher:
+		RequestDispatcher rd = request.getRequestDispatcher("go.jsp");
+		// send this request and response object to same server , but different
+		// servlet
+		// who knows how to deal with it:
+		rd.forward(request, response);
 	}
 }
